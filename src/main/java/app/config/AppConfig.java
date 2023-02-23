36af19b2b80a94585070e460faf822911746aeb3
volos <volosain@mail.ru>
2023-02-23 23:59:35 +0600

    result

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,22 @@
 package app.config;
 
+import app.model.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "dog")
+    public Animal getDog() {
+        Animal animal = new Dog();
+        return animal;
+    }
+
+    @Bean(name = "timer")
+    public Timer getTimer() {
+        Timer timer = new Timer();
+        return timer;
+    }
 }
